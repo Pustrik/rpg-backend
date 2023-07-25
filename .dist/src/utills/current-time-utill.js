878ad8585f7edb882360d02406d1266ab07f1de3
Pustrik <90838817+Pustrik@users.noreply.github.com>
2023-07-25 14:18:00 +0300

    Delete .dist directory

--- .dist/src/utills/current-time-utill.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const getCurrentDate = () => {
-    const date = new Date();
-    return date.toLocaleString();
-};
-exports.default = getCurrentDate;
-//# sourceMappingURL=current-time-utill.js.map
